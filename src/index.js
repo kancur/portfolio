5c7a9eeacaa2b187ee9669c2a920cb624b0401b2
Peter Smid <petosmid@gmail.com>
2021-12-04 17:48:32 +0100

    initial work

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 import smoothscroll from 'smoothscroll-polyfill';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import './styles/main.scss';
+import initSmoothScroll from './smoothscroll';
+import setFirstSectionHeight from './firstSectionHeightHandler';
 
+AOS.init({
+  offset: 60,
+});
 smoothscroll.polyfill();
 
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelectorAll('nav a[href^="#"]').forEach((element) => {
-    element.onclick = function (e) {
-      e.preventDefault();
-      let hash = this.getAttribute('href');
-      let target = document.querySelector(hash);
-      let elementPosition = target.offsetTop;
+  setFirstSectionHeight();
+  initSmoothScroll();
 
-      window.scrollTo({
-        top: elementPosition,
-        behavior: 'smooth',
-      });
-    };
+  let windowWidth = window.innerWidth;
+
+  window.addEventListener('resize', () => {
+    // only fire if the width has changed -- dont resize on mobile address bar hide/show
+    if (window.innerWidth === windowWidth) return;
+    windowWidth = window.innerWidth;
+    setFirstSectionHeight(window.innerHeight);
   });
+
+  const hamburger = document.querySelector('#hamburger');
+  const nav = document.querySelector('nav');
+
+  hamburger.addEventListener('click', () => toggleMenu())
+
+  nav.addEventListener('click', () => toggleMenu())
+
+  const toggleMenu = () => {
+    hamburger.classList.toggle('is-active');
+    nav.classList.toggle('show-mobile-menu');
+  };
 });
