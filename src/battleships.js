dbafbc945dbf8b5e1881891adf997f42a3014cea
Peter Smid <petosmid@gmail.com>
2022-01-05 00:07:26 +0100

    add randomizer function

---
+++ src/battleships.js
@@ -0,0 +1,13 @@
+export default function randomizeBattleships() {
+  const battleships = document.querySelectorAll('.battleship');
+  const maxDuration = 250;
+  const minDuration = 80;
+
+
+  for (let i = 0; i < battleships.length; i++) {
+    const animationDuration = Math.floor(Math.random() * (maxDuration - minDuration + 1) + minDuration);
+    const randomDuration = Math.floor(Math.random() * animationDuration);
+    battleships[i].style.animationDuration = `${animationDuration}s`;
+    battleships[i].style.animationDelay = -randomDuration + 's';  
+  }
+}
