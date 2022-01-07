bc6c94fe8ea6300f63177c7613d2ef3a996306ae
Peter Smid <petosmid@gmail.com>
2022-01-07 12:08:05 +0100

    add animate on scroll delays in JS

---
+++ src/aosDelays.js
@@ -0,0 +1,15 @@
+const INITIAL_DELAY = 100;
+const DIFFERENCE = 150;
+
+export default function setAosDelays(){
+  const techStacks = document.querySelectorAll('.techstack');
+
+  for (let stack of techStacks) {
+    const ul = stack.querySelector('ul');
+    const listItems = ul.querySelectorAll('li');
+    for (let i = 0; i < listItems.length; i++) {
+      const delay = INITIAL_DELAY + i * DIFFERENCE;
+      listItems[i].setAttribute('data-aos-delay', delay);
+    }
+  }
+}
