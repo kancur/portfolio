276ecf8a6c035761e8fa25c818f069212f3516b9
Peter Smid <petosmid@gmail.com>
2022-01-16 08:54:02 +0100

    rebuild

--- src/scripts/toolsy.js
+++ src/scripts/toolsy.js
@@ -5,7 +5,7 @@ export default function addRandomIconsToolsy() {
 
   for (let i = 0; i < NUMBER_OF_ICONS; i++) {
     const icon = document.createElement('img');
-    icon.src = '/toolsy-icon.png';
+    icon.src = 'toolsy-icon.png';
     icon.classList.add('decoration', 'toolsy-icon');
 
     icon.style.width = `${randomInt(10, 100)}px`;
