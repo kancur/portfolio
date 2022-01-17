169c72e4e1721f7044adfb6661dce24507ecb41a
Peter Smid <petosmid@gmail.com>
2022-01-17 17:26:59 +0100

    handle Reduced Motion

--- src/scripts/battleships.js
+++ src/scripts/battleships.js
@@ -1,13 +1,18 @@
-export default function randomizeBattleships() {
-  const battleships = document.querySelectorAll('.battleship');
-  const maxDuration = 300;
-  const minDuration = 80;
+import ReducedMotionHandler from "./ReducedMotionHandler";
 
+ReducedMotionHandler.registerCallback(() => {
+  randomizeBattleships(true)
+})
+
+export default function randomizeBattleships(reducedMotion = ReducedMotionHandler.isReducedMotion) {  
+  const minDuration = reducedMotion ? 30000 : 80
+  const maxDuration = reducedMotion ? 30000: 300
+  const battleships = document.querySelectorAll('.battleship');
 
   for (let i = 0; i < battleships.length; i++) {
     const animationDuration = Math.floor(Math.random() * (maxDuration - minDuration + 1) + minDuration);
     const randomDuration = Math.floor(Math.random() * animationDuration);
     battleships[i].style.animationDuration = `${animationDuration}s`;
-    battleships[i].style.animationDelay = -randomDuration + 's';  
+    battleships[i].style.animationDelay = -randomDuration + 's';
   }
 }
